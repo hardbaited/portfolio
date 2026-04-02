dde375070c65bdc44e17e7f84f0c5450b7c61b19
hardbaited <pantelisxanthos@gmail.com>
2026-04-02 13:48:30 +0300

    Change port

--- index.mjs
+++ index.mjs
@@ -7,7 +7,7 @@ async function loadApp() {
   const { app } = await import("./build/index.js");
 
   if (app?.listen) {
-    const port = process.env.PORT || 3000;
+    const port = process.env.PORT || 3001;
     app.listen(port, () => {
       console.log(`[server] Listening on port ${port}`);
     });
